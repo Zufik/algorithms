93b47c8510a79568d88dda21822395ac34be813f
Lenka Zufala <m-lenkazufala@seznam.cz>
2025-05-12 12:08:13 +0200

    yes

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-/*
 
-
-
-
-Spočítejte průměr všech čísel v poli.
-Spočítejte součet všech kladných čísel v poli.DONE
-*/
 //ALL NUMBERS
 const numbers = [
     -24, -11, 27, 29, -4, -28, -21, -14, 3, -8, 24, 19, -25, -2, -1, 11, 32, -31, 5
